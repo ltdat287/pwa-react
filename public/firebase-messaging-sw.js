a82207f38b498a0fba9158577141fc82a67810ef
Victor.Dat <ltdat5789@gmail.com>
2018-10-10 14:27:31 +0700

    fix firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,18 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../firebase-messaging-sw.js')
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    }).catch(function(err) {
-    console.log('Service worker registration failed, error:', err);
-  });
-}
+importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js");
+
+// For Firebase
+let config = {
+  messagingSenderId: "1057638247028"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(payload => {
+  const title = payload.notification.title;
+  console.log('payload', payload.notification.icon);
+  const options = {
+    body: payload.notification.body,
+    icon: payload.notification.icon
+  };
+  return self.registration.showNotification(title, options);
+});
--- public/worker.js
+++ public/worker.js
@@ -1,6 +1,3 @@
-importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js");
-
 // Flag for enabling cache in production
 var doCache = false;
 var CACHE_NAME = 'pwa-app-cache';
@@ -50,22 +47,6 @@ self.addEventListener('fetch', function (event) {
       })
     );
   }
-});
-
-// For Firebase
-let config = {
-  messagingSenderId: "1057638247028"
-};
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(payload => {
-  const title = payload.notification.title;
-  console.log('payload', payload.notification.icon);
-  const options = {
-    body: payload.notification.body,
-    icon: payload.notification.icon
-  };
-  return self.registration.showNotification(title, options);
 });
 
 self.addEventListener("notificationclick", function (event) {
